6c7250fb85d5ead5fea8226c6d1a91c59bcefecd
kimikimi714 <kimikimi714@gmail.com>
2015-07-27 23:54:15 +0900

    composerでautoloaderを作ってみるテスト

---
+++ app.php
@@ -0,0 +1,8 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+use kimikimi714\autoloader\Autoload;
+
+$autoload = new Autoload();
+$autoload->hello();
+
---
+++ autoload/AutoLoad/AutoLoad.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace kimikimi714\autoloader;
+
+class Autoload {
+  public function hello() {
+    var_dump('hello');
+  }
+}
+
